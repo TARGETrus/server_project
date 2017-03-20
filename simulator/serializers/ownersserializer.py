c95e10d53ce17511cf5b90bdd673fecedd2ef612
TARGETrus <mousebreaker@mail.ru>
2017-03-21 00:04:04 +0300

    Created 'owners' and 'realestate' prototype serializers.

---
+++ simulator/serializers/ownersserializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from simulator.models.owners import Owner, PhysicalEntity, LegalEntity
+
+
+class OwnerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Owner
+        fields = ('id', 'phone_number', 'address_actual', 'address_registered')
+
+
+class PhysicalEntitySerializer(OwnerSerializer):
+    class Meta(OwnerSerializer.Meta):
+        model = PhysicalEntity
+        fields = OwnerSerializer.Meta.fields + ('first_name', 'second_name', 'third_name', 'gender', 'birth_date',
+                                                'passport_series', 'passport_number', 'passport_issued_date')
+
+
+class LegalEntitySerializer(OwnerSerializer):
+    class Meta(OwnerSerializer.Meta):
+        model = LegalEntity
+        fields = OwnerSerializer.Meta.fields + ('company_name', 'inn')
---
+++ simulator/serializers/realestateserializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from simulator.models.realestate import RealEstate, Flat, Room
+
+
+class RealEstateSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RealEstate
+        fields = ('id', 'owner', 'for_rent')
+
+
+class FlatSerializer(RealEstateSerializer):
+    class Meta(RealEstateSerializer.Meta):
+        model = Flat
+        fields = RealEstateSerializer.Meta.fields + ('description', 'flat_type', 'per_room_basis', 'new_build')
+
+
+class RoomSerializer(RealEstateSerializer):
+    class Meta(RealEstateSerializer.Meta):
+        model = Room
+        fields = RealEstateSerializer.Meta.fields + ('parent_flat', 'room_type', 'square')
