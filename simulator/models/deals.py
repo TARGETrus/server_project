658da6db8978eaf38190f28f2006f9e71faf564c
TARGETrus <mousebreaker@mail.ru>
2017-04-08 21:45:07 +0300

    Deals updated.

--- simulator/models/deals.py
+++ simulator/models/deals.py
@@ -1,17 +1,35 @@
 from django.db import models
 
 from simulator.models.owners import Owner
+from simulator.models.realestate import RealEstate
+from simulator.utils.enums import DealClass
+
+DEAL_CLASS = {
+    (DealClass.DEAL.value, 'Deal'),
+    (DealClass.RENT.value, 'Rent'),
+    (DealClass.SALE.value, 'Sale')
+}
 
 
 class Deal(models.Model):
-    vendor = models.ForeignKey(Owner, on_delete=models.PROTECT, related_name='vendor')
-    customer = models.ForeignKey(Owner, on_delete=models.PROTECT, related_name='customer')
+    deal_class_type = models.CharField(choices=DEAL_CLASS, max_length=1, editable=False, default=DealClass.DEAL.value)
+    vendor = models.ForeignKey(Owner, related_name='deal', on_delete=models.PROTECT)
+    customer = models.ForeignKey(Owner, related_name='deal', on_delete=models.PROTECT)
+    real_estate = models.ForeignKey(RealEstate, related_name='deal', on_delete=models.PROTECT)
 
 
 class Sale(Deal):
     deal_date = models.DateField()
 
+    def save(self, *args, **kwargs):
+        self.deal_class_type = DealClass.SALE.value
+        super(Sale, self).save(*args, **kwargs)
+
 
 class Rent(Deal):
     from_date = models.DateField()
     till_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        self.deal_class_type = DealClass.RENT.value
+        super(Rent, self).save(*args, **kwargs)
