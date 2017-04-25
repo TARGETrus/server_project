5327b80af6b137bb7d02c276375ba0e836c1e5e6
TARGETrus <mousebreaker@mail.ru>
2017-04-25 11:52:32 +0300

    Admin site configured.

--- simulator/admin.py
+++ simulator/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from simulator.models.owners import PhysicalEntity, LegalEntity
+from simulator.models.realestate import Flat, Room
+from simulator.models.prices import Price
+from simulator.models.deals import Sale, Rent
+
+
+admin.site.register(PhysicalEntity)
+admin.site.register(LegalEntity)
+admin.site.register(Flat)
+admin.site.register(Room)
+admin.site.register(Price)
+admin.site.register(Sale)
+admin.site.register(Rent)
