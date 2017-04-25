2646f4b82c51aa32ed1bb32874c941ca3a198c61
TARGETrus <mousebreaker@mail.ru>
2017-04-25 22:44:48 +0300

    buttons added to single template entities.

--- simulator/views/views.py
+++ simulator/views/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html')
+
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        return redirect('/')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
